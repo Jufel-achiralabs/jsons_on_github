ad1fa2c9904acc799fb5732f88b8ddfd81be7dfb
Jufel_achira <jufel@achiralabs.com>
2025-07-07 23:07:02 +0530

    adding the first json and the code to make qrs based on the jsons, also adds a qr.yaml which tells github to automate the process of updating the qr codes if the jsons were to change in the future

---
+++ qr_batch.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+qr_batch.py – emit PNGs for every *.json in ./data
+pip install qrcode[pil] tqdm
+"""
+from pathlib import Path
+import qrcode, tqdm, subprocess, os
+
+BASE_URL = "https://cdn.jsdelivr.net/gh/jufel-achiralabs/jsons_on_github@main"  # pin later if you tag
+BASE_PATH = r"D:\Jufel\repos\jsons_on_github"
+
+for p in tqdm.tqdm(list(Path(os.path.join((BASE_PATH),"data")).rglob("*.json"))):
+    url = f"{BASE_URL}/{p.as_posix()}"
+    print(url)
+    out = Path(os.path.join((BASE_PATH),"qr")) / p.with_suffix(".png").name
+    print(out)
+    out.parent.mkdir(parents=True, exist_ok=True)
+    qrcode.make(url).save(out)
